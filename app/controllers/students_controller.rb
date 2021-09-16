31a4af13f8c00b53e82efa37c2d5b390a41cd24d
acm-sp85 <acontell@gmail.com>
2021-09-16 18:10:19 -0400

    commit tree 205664af6b607e25e85c65044f60c11b55c610e0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    
+    # students = Student.find_by(first_name: params[:name])
+
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
